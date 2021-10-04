8d2e70be6ae31fc7d2916d6045cf02166d68e24f
s464935 <makslas@interia.pl>
2021-10-04 18:23:20 +0200

    commit

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,15 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+    final static Logger logger = Logger.getLogger("HelloWorld.class");
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+
+        logger.info("Dzien dobry");
     }
+
 }
